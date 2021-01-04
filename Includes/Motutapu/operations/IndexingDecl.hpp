7ff29754c8acbe740194634f6f331bbf17fda0fe
Justin <jwkim98@kaist.ac.kr>
2021-01-04 19:13:05 +0900

    add : internal function declarations

---
+++ Includes/Motutapu/operations/IndexingDecl.hpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2021, Justin Kim
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef MOTUTAPU_INDEXING_DECL_HPP
+#define MOTUTAPU_INDEXING_DECL_HPP
+
+#include <Motutapu/util/ResourceManager.hpp>
+
+namespace Motutapu
+{
+
+}
+
+#endif
