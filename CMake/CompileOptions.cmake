b7f7bbd068f86a09908763472908b83e93f591db
Justin <jwkim98@kaist.ac.kr>
2021-01-05 19:32:24 +0900

    add : set cuda c++17 flag

--- CMake/CompileOptions.cmake
+++ CMake/CompileOptions.cmake
@@ -88,6 +88,7 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
 		${WARN_AS_ERROR_FLAGS}
 
 		/wd4819       # -> disable warning: The file contains a character that cannot be represented in the current code page (949) (caused by pybind11)
+		/wd4505		  # -> disable warning: 'function' : unreferenced local function has been removed
 
 		#$<$<CONFIG:Debug>:
 		#/RTCc        # -> value is assigned to a smaller data type and results in a data loss
