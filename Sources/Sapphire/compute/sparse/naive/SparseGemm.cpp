7dee699c6be406ba714bc6b0af91cabc9617b17c
jwkim98 <jwkimrhkgkr@gmail.com>
2021-05-13 00:47:22 +0900

    add : template for host side sparse matrix computation

---
+++ Sources/Sapphire/compute/sparse/naive/SparseGemm.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2021, Justin Kim
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <Sapphire/compute/sparse/naive/SparseGemm.hpp>
+#include <Sapphire/util/MemoryManager.hpp>
+
+namespace Sapphire::Compute::Sparse::Naive
+{
+void Gemm(SparseMatrix** output, SparseMatrix* a, SparseMatrix* b, uint32_t m,
+          uint32_t n, size_t numMatrices)
+{
+
+}
+}  // namespace Sapphire::Compute::Sparse::Naive
